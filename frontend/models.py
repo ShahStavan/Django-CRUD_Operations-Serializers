4042f1d09f05d72b74e0037620891201ad49c651
Shah Stavan <shahstavan72@gmail.com>
2022-01-18 23:42:03 +0530

    Created Post model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    content_1 = models.TextField(null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
